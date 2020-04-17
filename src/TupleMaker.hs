aa87aee896ae9354f057dc490915ced085425fe1
9032676 <9032676@gmail.com>
2020-04-18 01:40:24 +0800

    Update TupleMaker solution

--- src/TupleMaker.hs
+++ src/TupleMaker.hs
@@ -2,8 +2,18 @@
 module TupleMaker (tuple) where
 
 import Language.Haskell.TH
+import Control.Monad
 
 -- | Creates a lambda that takes `n` arguments and
 -- | returns an n-tuple of those arguments.
+
 tuple :: Int -> Q Exp
-tuple x = [| \y -> y + x + 1 |]
+tuple n = do
+    ps <- replicateM n $ newName "x"
+    return $ LamE (VarP <$> ps) $ TupE $ VarE <$> ps
+
+-- tuple' :: Int -> Q Exp
+-- tuple' x = f x []
+--     where
+--         f 0 r = return $ LamE (VarP <$> r) $ TupE $ VarE <$> r
+--         f n r = f (n-1) $ r ++ [mkName ("x" ++ show n)]
